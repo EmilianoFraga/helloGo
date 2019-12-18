5f2fafe141f957e15a570c4326524fc13cee8c5e
Emiliano Vaz Fraga <granamirdev@gmail.com>
2019-12-18 22:34:12 +0100

    More arrays and slices

--- arrays.go
+++ arrays.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func testArrays() {
-	arTen := [...] int{ 0, 1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9 }
+	var arTen = [...] int{ 0, 1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9 }
 	fmt.Println(varinfo(arTen))
 
 	var arFive [5] int
@@ -17,5 +17,34 @@ func testArrays() {
 	arThree[0] = "one"
 	arThree[1] = "two"
 	arThree[2] = "three"
+
+	var arThreeCopy = arThree
+	var arThreeRef = &arThree
+
 	fmt.Println(varinfo(arThree))
+	fmt.Println(varinfo(arThreeCopy))
+	fmt.Println(varinfo(arThreeRef))
+
+	arThreeCopy[1] = "abracadabra"
+	arThreeRef[1] = "mingau"
+
+	fmt.Println(varinfo(arThree))
+	fmt.Println(varinfo(arThreeCopy))
+	fmt.Println(varinfo(arThreeRef))
+}
+
+
+func testSlices() {
+	var arBack = [...] int{ 0, 1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9 }
+	var sliceFront []int = arBack[:]
+	anotherSlice := arBack[:]
+
+	fmt.Println(varinfo(arBack))
+	fmt.Println(varinfo(sliceFront))
+	fmt.Println(varinfo(anotherSlice))
+
+	sliceFront[2] = 2123
+	fmt.Println(varinfo(arBack))
+	fmt.Println(varinfo(sliceFront))
+	fmt.Println(varinfo(anotherSlice))
 }
