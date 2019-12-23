2b48a24bd4e0eb5662dc04ce1c4fcf8a33dd0d75
Emiliano Vaz Fraga <granamirdev@gmail.com>
2019-12-23 23:17:05 +0100

    First test file

---
+++ forLoop_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestForOne(t *testing.T) {
+	const expectedI = 500
+
+	i := expectedI
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("i = ", i)
+	}
+
+	if i != expectedI {
+		t.Errorf("Invalid %d expected as %d", i, expectedI)
+	}
+
+	for i := 9; i >= 0; i-- {
+		fmt.Println("i = ", i)
+	}
+
+	if i != expectedI {
+		t.Errorf("Invalid %d expected as %d", i, expectedI)
+	}
+
+	for i > 0 {
+		fmt.Println("i = ", i)
+
+		i /= 2
+	}
+
+	if i != 0 {
+		t.Errorf("Invalid %d expected as %d", i, 0)
+	}
+
+	slice := []int {12, 3, 49, 21, 44, 45}
+	for i, x := range slice {
+		if i > 0 { print(", ")}
+		fmt.Print("x = ", x)
+	}
+	fmt.Println()
+
+	if i != 0 {
+		t.Errorf("Invalid %d expected as %d", i, 0)
+	}
+}
