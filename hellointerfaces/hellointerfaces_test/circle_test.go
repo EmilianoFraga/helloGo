184c3a732e3197a3786758081aaa92e1a0b35339
Emiliano Vaz Fraga <granamirdev@gmail.com>
2019-12-31 11:09:06 +0100

    Add Error support to circle.go

--- hellointerfaces/hellointerfaces_test/circle_test.go
+++ hellointerfaces/hellointerfaces_test/circle_test.go
@@ -6,20 +6,24 @@ import (
 	"testing"
 )
 
+func TestCreateNewCircle(t *testing.T) {
+	assertCreateNewCircle(t, 2.0)
+
+	assertCreateNewCircle(t, 3.0)
+}
+
 func TestCircleSetters(t *testing.T) {
-	c := hellointerfaces.CreateNewCircle(2)
+	c, _ := hellointerfaces.CreateNewCircle(2.0)
 
-	expectedCircleRadius := 2.0
-	assertCircleRadius(t, c, expectedCircleRadius)
+	assertSetCircleRadius(t, c, 2.5)
 
-	c.SetRadius(3.0)
+	assertSetCircleRadius(t, c, 3.0)
 
-	expectedCircleRadius = 3.0
-	assertCircleRadius(t, c, expectedCircleRadius)
+	assertSetCircleRadiusError(t, c, -1.0)
 }
 
 func TestCircleArea(t *testing.T) {
-	c := hellointerfaces.CreateNewCircle(2)
+	c, _ := hellointerfaces.CreateNewCircle(2)
 
 	expectedCircleArea := math.Pi * (2.0 * 2.0)
 
@@ -28,15 +32,59 @@ func TestCircleArea(t *testing.T) {
 }
 
 func TestCirclePerim(t *testing.T) {
-	c := hellointerfaces.CreateNewCircle(2)
+	c, _ := hellointerfaces.CreateNewCircle(2)
 
 	expectedCirclePerim := (math.Pi + math.Pi) * 2.0
 
 	assertGeometryPerim(t, c, expectedCirclePerim)
 }
 
-func assertCircleRadius(t *testing.T, c hellointerfaces.Circle, expectedRadius float64) {
+func assertCreateNewCircle(t *testing.T, expectedRadius float64) {
+	c, ok := hellointerfaces.CreateNewCircle(expectedRadius)
+
+	if ok != nil {
+		t.Errorf("Error creating new Circle: %s", ok)
+	}
+
 	if actualRadius := c.GetRadius(); actualRadius != expectedRadius {
 		t.Errorf("Wrong circle radius %f, expected %f", actualRadius, expectedRadius)
+	}
+}
+
+func assertCreateNewCircleError(t *testing.T, invalidRadius float64) {
+	c, ok := hellointerfaces.CreateNewCircle(invalidRadius)
+
+	if ok == nil {
+		t.Errorf("Expected error while creating new circle with radius: %f", invalidRadius)
+	}
+
+	if c != nil {
+		t.Errorf("After error returned circle should be nil")
+	}
+}
+
+func assertSetCircleRadius(t *testing.T, c *hellointerfaces.Circle, expectedRadius float64) {
+	ok := c.SetRadius(expectedRadius)
+
+	if ok != nil {
+		t.Errorf("Error setting radius: %s", ok)
+	}
+
+	if actualRadius := c.GetRadius(); actualRadius != expectedRadius {
+		t.Errorf("Wrong circle radius %f, expected %f", actualRadius, expectedRadius)
+	}
+}
+
+func assertSetCircleRadiusError(t *testing.T, c *hellointerfaces.Circle, invalidRadius float64) {
+	previousRadius := c.GetRadius()
+
+	ok := c.SetRadius(invalidRadius)
+
+	if ok == nil {
+		t.Errorf("Expected error while setting radius: %f", invalidRadius)
+	}
+
+	if actualRadius := c.GetRadius(); actualRadius != previousRadius {
+		t.Errorf("Circle radius shouldn't change after error. Actual %f, expected %f", actualRadius, previousRadius)
 	}
 }
